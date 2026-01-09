8f1785ffa8dd1967d3734df2c35b2f945ea921c5
dongseok <sds0410@gmail.com>
2026-01-09 09:44:15 +0900

    scss 설정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import { useCounterStore } from '@/stores/counter'
+
+const counter = useCounterStore()
 </script>
 
 <template>
   <main>
+    <div>
+      <p>Count: {{ counter.count }}</p>
+      <p>Double: {{ counter.doubleCount }}</p>
+      <button @click="counter.increment">+1</button>
+    </div>
     <TheWelcome />
   </main>
 </template>
---
+++ src/views/TestView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const text = ref('')
+</script>
+
+<template>
+  <main>
+    <input v-model="text" placeholder="입력하세요" />
+    <p>{{ text }}</p>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  input {
+    border: 1px solid #ccc;
+
+    &:focus {
+      border-color: blue;
+    }
+  }
+
+  p {
+    color: white;
+  }
+}
+</style>
